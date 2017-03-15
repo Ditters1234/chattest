cd6d443c743d1e608f52bfc551e115532ae68d06
Nicholas Tinsley <Nicholas Tinsley>
2017-03-14 20:23:34 -0500

    update server stuff

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+
+
+ /* serves main page */
+ app.get("/", function(req, res) {
+    res.sendfile('index.html')
+ });
+ 
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile( __dirname + req.params[0]); 
+ });
 
 io.on('connection', function(socket){
-   console.log('new connection', socket.id);
-  socket.on('chat message', function(msg){
-	console.log('chat message', msg);
-    io.emit('chat message', msg);
-  });
+    console.log('new connection', socket.id);
+    socket.on('mouse',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+      
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+        
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+      }
+    );
+    
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+    });
 });
 
 http.listen(port, function(){
